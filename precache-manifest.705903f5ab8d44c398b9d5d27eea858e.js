d77198e7542885cb7da7e1bdb635d118f0f30634
Gio Talabong <gio.talabong@gmail.com>
2020-10-02 23:00:33 -0700

    Updates

---
+++ precache-manifest.705903f5ab8d44c398b9d5d27eea858e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc687fb71b1d6f0c951db6db7ca18e6d",
+    "url": "/gio-hw-19/index.html"
+  },
+  {
+    "revision": "431d1f8ee6721ceba4c5",
+    "url": "/gio-hw-19/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "55b430179b054a2dc736",
+    "url": "/gio-hw-19/static/css/main.b952ed59.chunk.css"
+  },
+  {
+    "revision": "431d1f8ee6721ceba4c5",
+    "url": "/gio-hw-19/static/js/2.314eec4a.chunk.js"
+  },
+  {
+    "revision": "55b430179b054a2dc736",
+    "url": "/gio-hw-19/static/js/main.3b0b9e3d.chunk.js"
+  },
+  {
+    "revision": "6a3302bd8b8ca3245f24",
+    "url": "/gio-hw-19/static/js/runtime~main.cb699a0b.js"
+  }
+]);
